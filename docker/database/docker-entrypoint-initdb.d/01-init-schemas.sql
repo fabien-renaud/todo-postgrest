ed3f0ddcdd7295c49cde73bd2e9b4d993b2be8aa
fabien_renaud <frd.frenaud@gmail.com>
2021-12-19 04:01:44 +0100

    Fix plurial

--- docker/database/docker-entrypoint-initdb.d/01-init-schemas.sql
+++ docker/database/docker-entrypoint-initdb.d/01-init-schemas.sql
@@ -1,11 +1,11 @@
 create schema todo_api;
 
-create table todo_api.todo (
+create table todo_api.todos (
   id serial primary key,
   done boolean not null default false,
   task text not null,
   due timestamptz
 );
 
-insert into todo_api.todo (task) values
+insert into todo_api.todos (task) values
   ('finish tutorial 0'), ('pat self on back');
--- docker/database/docker-entrypoint-initdb.d/02-init-roles.sql
+++ docker/database/docker-entrypoint-initdb.d/02-init-roles.sql
@@ -7,7 +7,7 @@ GRANT SELECT ON ALL TABLES IN SCHEMA todo_api TO anonymous;
 CREATE ROLE authenticated_user NOLOGIN;
 GRANT USAGE ON SCHEMA todo_api TO authenticated_user;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA todo_api TO authenticated_user;
-GRANT USAGE, SELECT ON SEQUENCE todo_api.todo_id_seq TO authenticated_user;
+GRANT USAGE, SELECT ON SEQUENCE todo_api.todos_id_seq TO authenticated_user;
 
 -- Create user authenticator
 CREATE ROLE authenticator NOINHERIT LOGIN PASSWORD 'postgres';
